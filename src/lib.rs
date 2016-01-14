95678545456f5610510b6052d204cf8a7db83792
Matt Kraai <kraai@ftbfs.org>
2016-01-13 20:06:02 -0800

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,12 @@ use std::io::Read;
 use hyper::Client;
 use hyper::header::Connection;
 
-pub fn test_api() {
-    println!("Hello, Stockfighter!");
-
+fn get<U: hyper::client::IntoUrl>(url: U) -> serde_json::Value {
     // Create a client.
     let client = Client::new();
 
     // Creating an outgoing request.
-    let mut res = client.get("http://api.stockfighter.io/ob/api/heartbeat")
+    let mut res = client.get(url)
         // set a header
         .header(Connection::close())
         // let 'er go!
@@ -24,26 +22,16 @@ pub fn test_api() {
     res.read_to_string(&mut body).unwrap();
 
     let value: serde_json::Value = serde_json::from_str(&body).unwrap();
+    value
+}
+
+pub fn test_api() {
+    let value = get("http://api.stockfighter.io/ob/api/heartbeat");
     println!("Response: {}", value.as_object().unwrap().get("ok").unwrap().as_boolean().unwrap());
 }
 
 pub fn test_venue(venue: &str) {
-    // Create a client.
-    let client = Client::new();
-
     let url = format!("https://api.stockfighter.io/ob/api/venues/{}/heartbeat", venue);
-
-    // Creating an outgoing request.
-    let mut res = client.get(&url)
-        // set a header
-        .header(Connection::close())
-        // let 'er go!
-        .send().unwrap();
-
-    // Read the Response.
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-
-    let value: serde_json::Value = serde_json::from_str(&body).unwrap();
+    let value = get(&url);
     println!("Response: {}", value.as_object().unwrap().get("ok").unwrap().as_boolean().unwrap());
 }
